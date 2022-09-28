5a83791c5264e76202152e806c54591173272848
lilly-lizard <bigbanana29@aol.com>
2022-09-28 16:50:23 +1300

    added support for including files in shaders

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
-use shaderc::{Compiler, ShaderKind};
+use shaderc::{CompileOptions, Compiler, IncludeType, ResolvedInclude, ShaderKind};
 use std::fs::File;
 use std::io::{Read, Write};
+use std::path::PathBuf;
 
 /// Compile glsl shaders in src/shaders and output spirv binaries to assets/shader_binares.
 /// Requirements:
@@ -12,12 +13,10 @@ fn gen_shader_spirv() {
     println!("Generating spirv shaders...");
 
     // shader source directory
-    let mut shader_dir = std::env::current_dir().expect("shouldn't panic: pwd must exist");
-    shader_dir.push("src");
-    shader_dir.push("shaders");
+    let shader_dir = get_shader_dir();
 
     // output spirv directory
-    let mut spirv_dir = std::env::current_dir().expect("shouldn't panic: pwd must exist");
+    let mut spirv_dir = std::env::current_dir().expect("cannot access pwd");
     spirv_dir.push("assets");
     spirv_dir.push("shader_binaries");
     assert!(spirv_dir.is_dir(), "invalid spirv destination path");
@@ -61,33 +60,68 @@ fn gen_shader_spirv() {
             _ => continue,
         };
 
-        // no more 'continue's
+        // no more `continue`s
         println!("Compiling {:?}...", file_name);
 
         // read shader source
-        let mut shader_text = String::new();
-        let mut shader_file =
-            File::open(&shader_path).expect("failed to open shader source file for reading");
-        shader_file
-            .read_to_string(&mut shader_text)
-            .expect("invalid utf-8 in shader source code");
+        let shader_text =
+            read_shader(&shader_path).expect(&format!("failed to read shader file {}", file_name));
 
-        // compile spirv
-        let spirv_comp =
-            match compiler.compile_into_spirv(&shader_text, shader_stage, &file_name, "main", None)
-            {
-                Ok(comp) => comp,
-                Err(e) => {
-                    panic!("failed to compile {} to spirv:\n{}", file_name, e);
-                    /*
-                    println!(
-                        "cargo:warning=failed to compile {} to spirv:\n{}",
-                        file_name, e
-                    );
-                    continue;
-                    */
-                }
-            };
+        // how to handle included files
+        let mut preprocess_options =
+            CompileOptions::new().expect("error initializing compile options object");
+        preprocess_options.set_include_callback(
+            |included_file_name: &str,
+             _include_type: IncludeType,
+             includer_file_name: &str,
+             _include_depth: usize| {
+                // determine included file path
+                let mut path = get_shader_dir();
+                path.push(included_file_name);
+                // read included file
+                let shader_text = match read_shader(&path) {
+                    Ok(x) => x,
+                    Err(e) => {
+                        return Err(format!(
+                            "failed to read shader file {} included in {}, due to error: {}",
+                            includer_file_name, included_file_name, e
+                        ))
+                    }
+                };
+                // return included source
+                Ok(ResolvedInclude {
+                    resolved_name: path.to_str().expect("invalid unicode").into(),
+                    content: shader_text,
+                })
+            },
+        );
+
+        // preprocess source to support including other shader files
+        let entry_point = "main";
+        let prep = match compiler.preprocess(
+            &shader_text,
+            &file_name,
+            &entry_point,
+            Some(&preprocess_options),
+        ) {
+            Ok(prep) => prep,
+            Err(e) => panic!("failed to preprocess {}:\n{}", file_name, e),
+        };
+
+        // compile to spirv
+        let spirv_comp = match compiler.compile_into_spirv(
+            &prep.as_text(),
+            shader_stage,
+            &file_name,
+            &entry_point,
+            None,
+        ) {
+            Ok(comp) => comp,
+            Err(e) => panic!("failed to compile {} to spirv:\n{}", file_name, e),
+            // alternatively just warn and carry on:
+            //println!("cargo:warning=failed to compile {} to spirv:\n{}", file_name, e);
+            //continue;
+        };
         let spirv_bin = spirv_comp.as_binary_u8();
 
         // write spirv to file
@@ -99,6 +133,22 @@ fn gen_shader_spirv() {
             .write_all(spirv_bin)
             .expect("failed to write spirv data to output file");
     }
+}
+
+/// Attempts to read the source file at `shader_path` and returns its contents as a String
+fn read_shader(shader_path: &PathBuf) -> std::io::Result<String> {
+    let mut shader_text = String::new();
+    let mut shader_file = File::open(shader_path)?;
+    shader_file.read_to_string(&mut shader_text)?;
+    Ok(shader_text)
+}
+
+/// Returns the directory containing the shader source files
+fn get_shader_dir() -> PathBuf {
+    let mut shader_dir = std::env::current_dir().expect("cannot access pwd");
+    shader_dir.push("src");
+    shader_dir.push("shaders");
+    shader_dir
 }
 
 fn main() {
---
+++ src/shaders/primitives.glsl
@@ -0,0 +1,5 @@
+
+const uint PRIMITIVE_UNIT_LENGTH = 8;
+const uint NULL = 0xFFFFFFFF;
+const uint SPHERE = 0xBFFFFFFF;
+const uint BOX = 0x7FFFFFFF;
